315817af6d5538500ff93aed541b0c2e48eaae4e
Shaunak <gamershaunj@gmail.com>
2024-05-18 13:26:40 -0400

    added initial components

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className = 'app'>
+      <div className = 'navbar'>
+        <Navbar />
+      </div>
+      <div className = 'main'>
+        
+      </div>
+      <div className = 'footer'>
+        
+      </div>
+        
+    </div>
+  )
+}
+
+export default App
